57d2c36bb556ce4aafb5e65da56627bf64b0b44e
bpadros <bpadros@gmail.com>
2022-07-07 22:06:37 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App  () {
+
+  const STARTING_TIME = 15
+
+  const [text,setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const[isTimeRunning,setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+  const textBoxRef = useRef(null)
+
+  function handleChange (e) {
+    const {value} = e.target
+    setText(value)
+    
+  }
+
+  
+function calculateWordCount(text) {
+const wordsArr = text.trim().split(" ")
+// console.log(wordsArr.length)
+return wordsArr.filter(word => word !== "").length
+}
+function startGame() {
+  setIsTimeRunning(true)
+  setTimeRemaining(STARTING_TIME)
+  setText("")
+  textBoxRef.current.disabled = false
+  textBoxRef.current.focus()
 }
 
-export default App;
+function endGame() {
+  setIsTimeRunning(false)
+  setWordCount(calculateWordCount(text))
+}
+
+
+
+
+
+useEffect(() => {
+  if(isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+          setTimeRemaining(time => time - 1)
+      }, 1000)
+  } else if(timeRemaining === 0){
+   endGame()
+  }
+}, [timeRemaining, isTimeRunning])
+
+
+
+
+
+
+return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea
+      ref={textBoxRef}
+      disabled={!isTimeRunning}
+      onChange={handleChange} 
+      value={text}/>
+      <h4>Time remaining : {timeRemaining}</h4>
+      <button 
+      onClick={startGame}
+      disabled={isTimeRunning}
+      >Start</button>
+      <h1>Word count : {wordCount}</h1>
+    </div>
+  );
+};
+
